d1b205fe14e601516d635cce8fd785fc59d26a02
segrallx <segrallx@gmail.com>
2024-03-03 09:56:41 +0800

    xx

---
+++ Assets/Scripts/Player/PlayerCtroller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtroller : MonoBehaviour
+{
+    // Start  update
+    void Start()
+    {
+    }
+
+    void TestFunc()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
