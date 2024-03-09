51505131348f4a26e4e68a125bcc69d462218fff
segrallx <segrallx@gmail.com>
2024-03-09 09:33:39 +0800

    crouch

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -7,12 +7,16 @@ public class PlayerAnimation : MonoBehaviour
 
     private Animator mAnim;
     private Rigidbody2D mRb;
+    private PhysicCheck mPhysicCheck;
+    private PlayerCtroller mPlayerCtrler;
 
     // Start is called before the first frame update
     private void Awake()
     {
         mAnim = GetComponent<Animator>();
         mRb = GetComponent<Rigidbody2D>();
+        mPhysicCheck = GetComponent<PhysicCheck>();
+        mPlayerCtrler = GetComponent<PlayerCtroller>();
     }
 
     // Update is called once per frame
@@ -24,5 +28,8 @@ public class PlayerAnimation : MonoBehaviour
     public void  SetAnimation()
     {
         mAnim.SetFloat("velocity_x", Mathf.Abs(mRb.velocity.x));
+        mAnim.SetFloat("velocity_y", (mRb.velocity.y));
+        mAnim.SetBool("isground", mPhysicCheck.mIsGround);
+        mAnim.SetBool("isCrouch", mPlayerCtrler.mIsCrouch);
     }
 }
--- Assets/Scripts/Player/PlayerCtroller.cs
+++ Assets/Scripts/Player/PlayerCtroller.cs
@@ -6,6 +6,7 @@ public class PlayerCtroller : MonoBehaviour
     public PlayerInputController mInputController;
     public Vector2 mInputDirection;
     private Rigidbody2D mRigidbody;
+    private CapsuleCollider2D mColl;
 
     //速度
     [Header("基本逻辑")]
@@ -15,7 +16,12 @@ public class PlayerCtroller : MonoBehaviour
     private PhysicCheck mPhysicCheck;
 
     private float mRunSpeed;
-    private float mWalkSpeed ;// => mSpeed/2.5f;
+    private float mWalkSpeed;// => mSpeed/2.5f;
+
+    // 是否下蹲
+    public bool mIsCrouch;
+    private Vector2 mCollOriginOffset;
+    private Vector2 mCollOriginSize;
 
 
     private void Awake()
@@ -23,24 +29,32 @@ public class PlayerCtroller : MonoBehaviour
         mInputController = new PlayerInputController();
         mRigidbody = GetComponent<Rigidbody2D>();
         mPhysicCheck = GetComponent<PhysicCheck>();
+        mColl = GetComponent<CapsuleCollider2D>();
+
+        mCollOriginOffset = mColl.offset;
+        mCollOriginSize = mColl.size;
 
         mInputController.Gameplay.Jump.started += Jump;
 
 
         #region 强制走路
         mRunSpeed = mSpeed;
-        mWalkSpeed = mSpeed/2.5f;
+        mWalkSpeed = mSpeed / 2.5f;
 
         // 按钮按住
-        mInputController.Gameplay.WalkButton.performed +=  ctx => {
-            if(mPhysicCheck.mIsGround) {
+        mInputController.Gameplay.WalkButton.performed += ctx =>
+        {
+            if (mPhysicCheck.mIsGround)
+            {
                 mSpeed = mWalkSpeed;
             }
         };
 
         // 按钮抬起
-        mInputController.Gameplay.WalkButton.canceled +=  ctx => {
-            if(mPhysicCheck.mIsGround) {
+        mInputController.Gameplay.WalkButton.canceled += ctx =>
+        {
+            if (mPhysicCheck.mIsGround)
+            {
                 mSpeed = mRunSpeed;
             }
         };
@@ -72,31 +86,49 @@ public class PlayerCtroller : MonoBehaviour
     public void Move()
     {
         // move left & right
-        mRigidbody.velocity = new Vector2(
-            mSpeed * mInputDirection.x * Time.deltaTime, mRigidbody.velocity.y);
+        if (!mIsCrouch)
+        {
+            mRigidbody.velocity = new Vector2(
+                mSpeed * mInputDirection.x * Time.deltaTime, mRigidbody.velocity.y);
+        }
 
         int faceDir = (int)transform.localScale.x;
-        if(mInputDirection.x >0 ) {
+        if (mInputDirection.x > 0)
+        {
             faceDir = 1;
         }
 
-        if(mInputDirection.x <0 ) {
+        if (mInputDirection.x < 0)
+        {
             faceDir = -1;
         }
         // flip
-        transform.localScale =  new Vector3(faceDir,1,1);
-
+        transform.localScale = new Vector3(faceDir, 1, 1);
         mVelocityX = mRigidbody.velocity.x;
+
+        // 下端
+        mIsCrouch = mInputDirection.y < -0.5f && mPhysicCheck.mIsGround;
+        if (mIsCrouch)
+        {
+            // 修改碰撞体
+            mColl.offset = new Vector2(-0.05f, 0.85f);
+            mColl.size = new Vector2(0.7f, 1.7f);
+        }
+        else
+        {
+            // 还原碰撞体
+            mColl.offset = mCollOriginOffset;
+            mColl.size = mCollOriginSize;
+        }
     }
 
     private void Jump(InputAction.CallbackContext obj)
     {
         Debug.Log("JUMP");
-        if(mPhysicCheck.mIsGround)
+        if (mPhysicCheck.mIsGround)
         {
             mRigidbody.AddForce(transform.up * mJumpForce, ForceMode2D.Impulse);
         }
-
     }
 
 
