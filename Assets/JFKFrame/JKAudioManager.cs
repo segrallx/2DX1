6bcc9e0f4259b34d227ab9f0ab2dc9c7bddddfb1
segrallx <segrallx@gmail.com>
2024-05-07 09:11:38 +0800

    audio

---
+++ Assets/JFKFrame/JKAudioManager.cs
@@ -0,0 +1,88 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JKAudioManager : ManageBase<JKAudioManager>
+{
+    [SerializeField]
+    private AudioSource bgAudioSource;
+
+    [SerializeField]
+    private AudioSource vfxAudioSource;
+
+    [SerializeField]
+    [Range(0,1)]
+    [OnValueChanged("UpdateGlobalAudioPlay")]
+    private float globalVolume;
+    public float GlobalVolume { get => globalVolume; set  { globalVolume = value; UpdateGlobalAudioPlay(); } }
+
+    [SerializeField]
+    [Range(0, 1)]
+    [OnValueChanged("UpdateBGAudioPlay")]
+    private float bgVolume;
+    public float BgVolume { get => bgVolume; set { bgVolume = value; UpdateGlobalAudioPlay(); } }
+
+    [SerializeField]
+    [Range(0, 1)]
+    [OnValueChanged("UpdateVfxAudioPlay")]
+    private float vfxVolume;
+    public float VfxVolume { get => vfxVolume; set { vfxVolume = value;  UpdateGlobalAudioPlay(); } }
+
+
+    [SerializeField]
+    [OnValueChanged("UpdateMute")]
+    private bool isMute = false;
+    public bool IsMute { 
+        get => isMute;
+        set
+        {
+            isMute = value;
+            UpdateMute();
+        }
+    }
+
+    private void UpdateMute()
+    {
+        bgAudioSource.mute = isMute;
+        UpdateVfxAudioPlay();
+    }
+
+    [SerializeField]
+    [OnValueChanged("UpdateLoop")]
+    private bool isLoop = true;
+    public bool IsLoop
+    {
+        get => isLoop;
+        set
+        {
+            isLoop = value;
+//            bgAudioSource.loop = isLoop;
+        }
+    }
+
+    private void UpdateLoop()
+    {
+        bgAudioSource.loop = isLoop;
+    }
+
+
+    private void UpdateBGAudioPlay()
+    {
+        bgAudioSource.volume = bgVolume * globalVolume;
+    }
+
+    private void UpdateVfxAudioPlay()
+    {
+        //vfxAudioSource.volume = vfxVolume * globalVolume;
+        Debug.Log("UpdateVfxAudioPlay");
+    }
+
+    private void UpdateGlobalAudioPlay()
+    {
+        Debug.Log("UpdateGlobalAudioPlay");
+        UpdateBGAudioPlay();
+        UpdateVfxAudioPlay();
+    }
+
+}
